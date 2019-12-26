bb87225739caefb61f854b46f8c375846cbae6c4
Ajar-maharjan <pleiades.ajar@gmail.com>
2019-12-26 11:39:07 +0545

    design

--- app/src/main/java/com/novc21/esoftwarica/MainActivity.java
+++ app/src/main/java/com/novc21/esoftwarica/MainActivity.java
@@ -1,8 +1,16 @@
 package com.novc21.esoftwarica;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.novc21.esoftwarica.fragment.AboutFragment;
+import com.novc21.esoftwarica.fragment.AddFragment;
+import com.novc21.esoftwarica.fragment.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+
+            switch (menuItem.getItemId()){
+                case R.id.navHome:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.navAddStudent:
+                    selectedFragment = new AddFragment();
+                    break;
+                case R.id.navAboutUs:
+                    selectedFragment = new AboutFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+        return true;
+        }
+    };
 }
